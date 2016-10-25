30450c48f74017bfb3912b6bea3f6e7dc43eae67
Haisheng.Wu.Okta <haisheng.wu@okta.com>
2016-10-25 11:25:42 -0700

    init

---
+++ src/Lib.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+
+module Lib
+    ( someFunc
+    ) where
+
+--import Data.Typeable
+import Diagrams.Prelude
+--import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Backend.Rasterific.CmdLine
+
+
+-- someFunc :: (TypeableFloat n, Typeable a, Renderable (DImage n a) b) => IO (QDiagram b V2 n Any)
+someFunc :: IO ()
+someFunc = do
+  img <- loadImageIO
+  mainWith ((img ||| img ) === (img ||| img ) :: Diagram B)
+
+-- TODO: ImageEmb .v.s loadImageExt
+--
+loadImageIO = do
+  res <- loadImageEmb "/Users/haisheng.wu/git/play-images/data/hw.jpg"
+  return $ case res of
+     Left err    -> mempty
+     Right phone -> image phone
